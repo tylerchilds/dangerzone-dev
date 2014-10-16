056aaa4ab8ef53d84e77e0a7fba38a2f0c135a1b
Brandon Kreisel <kreiselb@gmail.com>
2014-10-15 23:58:32 -0400

    Add up and down functionality

--- dangerzone.sh
+++ dangerzone.sh
@@ -8,16 +8,41 @@ help () { #Show Help Function
 }
 
 up() { #Start Development Enviornment
+
+    #Pull Latest Image
+    sudo docker pull bkreisel/bazaar-nginx
     if [ ! -e "etc/dangerzone.conf" ]; then
         echo "Yo! no config file."
         echo "Run: dangerzone.sh config"
         exit 1
     fi
 
-    GIT=`cat etc/dangerzone.conf | grep "GIT"`
+    GIT=`cat etc/dangerzone.conf | grep "GIT" | awk '{print $2}'`
     GIT=`eval "echo $GIT"`
-    PORT=`cat etc/dangerzone.conf | grep "PORT"`
+    PORT=`cat etc/dangerzone.conf | grep "PORT" | awk '{print $2}'`
     PORT=`eval "echo $PORT"`
+
+    #Check for Pre-existing enviornment
+    down
+
+    #Start the engines
+    echo "docker run -d -p $PORT:80 --name hermes-nginx -v $GIT:/app"
+    sudo docker run -d -p $PORT:80 --name hermes-nginx -v $GIT:/app bkreisel/bazaar-nginx
+    echo "Containers online, you are cleared for takeoff"
+}
+
+down() {
+    prevContain=`sudo docker ps -a | awk '{ print $1" "$(NF) }' | grep 'hermes-nginx' | awk '{ print $(NF)}'`
+    prevContainRunning=`sudo docker ps | awk '{ print $1" "$(NF) }' | grep 'hermes-nginx' | awk '{ print $(NF) }'`
+    if [  -n "$prevContainID" ]; then
+        #check for running container
+        if [  -n "$prevContainRunning" ]; then
+            echo "running container found"
+            sudo docker kill $prevContainRunning
+        fi
+            echo "previous container found"
+            sudo docker rm $prevContain
+    fi
 }
 
 config() {
@@ -43,8 +68,6 @@ config() {
     echo "Configuration Complete!"
 }
 
-
-
 #Do Work
 if [ $# -eq 0 ]; then
     echo "No Arguments Supplied ..."
@@ -64,4 +87,7 @@ elif [ "$1" = "up" ]; then
     up
 elif [ "$1" = "help" ]; then
     help
+elif [ "$1" = "down" ]; then
+    down
+    echo "Containers shipped to a far away port"
 fi
